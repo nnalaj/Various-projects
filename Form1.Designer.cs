d3d6c44289b481a86c5461de9e2421ed3bff1c42
scruf <scruf@ICEMACHINE2>
2023-09-25 00:22:54 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace helloworldremix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.videoBox = new AxWMPLib.AxWindowsMediaPlayer();
+            this.play = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.videoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // videoBox
+            // 
+            this.videoBox.Enabled = true;
+            this.videoBox.Location = new System.Drawing.Point(200, 55);
+            this.videoBox.Name = "videoBox";
+            this.videoBox.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("videoBox.OcxState")));
+            this.videoBox.Size = new System.Drawing.Size(415, 267);
+            this.videoBox.TabIndex = 0;
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(217, 346);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 23);
+            this.play.TabIndex = 2;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(370, 346);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(517, 346);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(784, 381);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.videoBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer videoBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace helloworldremix
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            videoBox.uiMode = "none";
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            string videoPath = @"C:\Users\scruf\Downloads\groove battle.mp4";
+            videoBox.URL = videoPath;
+            
+            videoBox.Ctlcontrols.play();
+            videoBox.uiMode = "none";
+            
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            videoBox.Ctlcontrols.stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
