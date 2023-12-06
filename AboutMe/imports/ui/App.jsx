2d442d24c39c2bbb8ee5c98e3b930db7fee2b970
nnalaj <jalannlittle923@gmail.com>
2023-12-06 14:13:40 -0500

    added new vids for the background

--- AboutMe/imports/ui/App.jsx
+++ AboutMe/imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Video  from './Video.jsx';
 import NNALAJ from './NNALAJ.jsx'; 
-
+import {Tracker} from 'meteor/tracker'; //implement this to refresh the video component after x time
 
 
 //let navigate = useNavigate();
--- AboutMe/imports/ui/Video.jsx
+++ AboutMe/imports/ui/Video.jsx
@@ -6,16 +6,31 @@ import Heading from "./NNALAJ";
 export default Video = () =>{
 
     const playlist = []
-    var i = 0
+   
     playlist.push ("./ollie.mp4")
     playlist.push ("./00530.mp4")
     playlist.push ("./20210109 big tree fire.mp4")
+    playlist.push ("./bs polejam.mp4")
+    playlist.push ("./half-cab heel.mp4")
+
+    // playlist.push ("https://photos.app.goo.gl/urxPduhixi66LG159")
+    // playlist.push ("https://photos.app.goo.gl/fEmdwuzGhLvnmcLW6")
+    // playlist.push ("https://photos.app.goo.gl/LQmrxey7HnNTGMBs6")
 
     //random num gen soon
+
+    function randomInt() {
+        var i = 0
+        var max = 5
+        i = Math.floor(Math.random() * max);
+        
+        return i;
+    }
+    
     
     return(
             <div className="video-background">
-                <video src= {playlist[2]} type='video/mp4' autoPlay loop muted/>
+                <video src= {playlist[randomInt()]} type='video/mp4' autoPlay loop muted/>
             </div>
     );
 };
