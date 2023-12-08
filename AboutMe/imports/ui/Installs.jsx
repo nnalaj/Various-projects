9095f368c60f85e63830d4862dd4503c9391470f
nnalaj <jalannlittle923@gmail.com>
2023-12-08 15:50:42 -0500

    added pages for merch and video installation info

---
+++ AboutMe/imports/ui/Installs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default Installs = () => {
+
+
+    return (
+        <div> 
+            <div className = "installsContent">
+                
+            </div>
+        </div>
+    )
+}
---
+++ AboutMe/imports/ui/Merch.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default Merch = () => {
+
+    return (
+        <div>
+            <h2 className = "merchHeader">v--v</h2>
+                <span>coming soon</span>
+            <div className = "merchTable">
+                <tr>
+                    <tc>
+                        <div>
+                            <img src="" width = "480" height = "240"></img>
+                        </div>
+                    </tc>
+                </tr>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
