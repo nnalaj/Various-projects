54b4ec165ef91c238e546e83fe49f27ae1167588
nnalaj <jalannlittle923@gmail.com>
2023-12-27 03:57:06 -0500

    started routing implementation

--- AboutMe/imports/ui/Nav.jsx
+++ AboutMe/imports/ui/Nav.jsx
@@ -3,11 +3,12 @@ import NNALAJ from "./NNALAJ";
 
 export default Nav = () => {
 
-   //simple grid for now need to find a way to animate the secontions or something
-   //TODO: link resume or add resume component somehow
+   //simple grid for now need to find a way to animate the secontions or something(Done, could use more work)
+   //TODO: link resume or add resume component somehow (working on)
    //TODO: link video installation works
    //TODO: link soundcould (but clean it first)
    //TODO: link youtube (also maybe clean this or make more videos first)
+   //TODO: implement router
    
     return (
         <div className = "homeGrid">
@@ -15,7 +16,7 @@ export default Nav = () => {
                 <div className = "cell" ></div>
             </section>
             <section>
-                <div className = "cell">Resume</div>
+            <div className = "cell"><a href = "./"></a></div>
             </section>
             <section>
                 <div className = "cell">
