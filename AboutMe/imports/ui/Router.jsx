7a42ace49862c503644499b468571048664bca7c
nnalaj <jalannlittle923@gmail.com>
2023-12-29 02:50:45 -0500

    udpdated router

--- AboutMe/imports/ui/Router.jsx
+++ AboutMe/imports/ui/Router.jsx
@@ -1,19 +1,34 @@
+import Resume from "./Resume";
+import Installs from "./Installs";
+import Home from "./Home";
+
 const Router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+        children: [
+            {
+                path: "/home",
+                element: <Home />,
+                errorElement: <ErrorPage />
+              },
+              {
+                path: "/installs",
+                element: <Installs />,
+                errorElement: <ErrorPage />
+              }, 
+              {
+                path: "/resume",
+                element: <Resume />,
+                errorElement: <ErrorPage />
+              }, 
+        ],
+
       errorElement: <ErrorPage />
+        
     },
-    {
-      path: "/home",
-      element: <Home />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path: "/installs",
-      element: <Installs />,
-      errorElement: <ErrorPage />
-    }, 
+    ,
+    
     
   ]);
 
